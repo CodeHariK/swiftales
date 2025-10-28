045005e71ce1ca9617d7f53611d1cddb6c053b05
Hari Krishnan <codeharik@gmail.com>
2025-10-28 21:25:07 +0530

    Initial Commit

---
+++ swiftales/swiftalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftalesApp.swift
+//  swiftales
+//
+//  Created by Shark on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
